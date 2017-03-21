14c608c0a9512aefa85b5f52c19b3cbf5f1377f4
kroteDev <oliveira.desu@gmail.com>
2017-03-21 12:58:35 -0300

    .gitignore updated

--- app/views/activities/_activity.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! activity, :id, :title, :description, :day, :duration, :priority, :client_id, :created_at, :updated_at
-json.url activity_url(activity, format: :json)
--- db/migrate/20170309133043_create_clients.rb
+++
@@ -1,9 +0,0 @@
-class CreateClients < ActiveRecord::Migration
-  def change
-    create_table :clients do |t|
-      t.string :nome
-
-      t.timestamps null: false
-    end
-  end
-end
