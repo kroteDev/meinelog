93898eb7b45ccb0f1ee274dc7cc4a615eb5e9d13
kroteDev <oliveira.desu@gmail.com>
2017-03-21 13:05:03 -0300

    git updated

---
+++ db/migrate/20170309133100_create_activities.rb
@@ -0,0 +1,15 @@
+class CreateActivities < ActiveRecord::Migration
+  def change
+    create_table :activities do |t|
+      t.string :title
+      t.text :description
+      t.datetime :day
+      t.time :duration
+      t.string :priority
+      t.boolean :complete
+      t.references :client, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
