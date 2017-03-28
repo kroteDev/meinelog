785e4dd84948a97ba981a37fda341161e7423104
kroteDev <oliveira.desu@gmail.com>
2017-03-28 12:21:38 -0300

    add more styles

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 	def create		
 		user = User.from_omniauth(request.env['omniauth.auth'])
 		session[:user_id] = user.id
-		redirect_to root_url
+		redirect_to activities_path
 	end
 	def destroy
     session[:user_id] = nil
