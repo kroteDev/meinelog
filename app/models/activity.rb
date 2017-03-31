51fd551d8fdb7810f9edac562b1ad41d6f887849
bolliveira <boliveira@4tuna.com.br>
2017-03-31 11:38:45 -0300

    added twitter sign in

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,4 +2,6 @@ class Activity < ActiveRecord::Base
   belongs_to :client
   scope :status, -> (complete)  { where complete: complete }
   scope :client, -> (client_id) { where client_id: client_id }
+  scope :priority, -> (priority)  { where priority: priority }
+  validates_presence_of :title, :priority,  :client_id
 end
