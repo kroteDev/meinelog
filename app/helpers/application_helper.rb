e8405ef9a6d61d59fbf77a3d676a8e49a0f4c9bc
bolliveira <boliveira@4tuna.com.br>
2017-04-02 01:29:41 -0300

    added delete option on activities

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+	def title(page_title)
+	  content_for :title, page_title.to_s
+	end
+
+	def og_url(url)
+		content_for :og_url, 'http://www.meinelog.krotedev.com.br/'+url
+	end
+
+	def og_description(description)
+	  content_for :og_description, description.to_s
+	end
+	
+	def og_image(image)
+	  content_for :og_image, 'http://www.meinelog.krotedev.com.br/assets/'+image
+	end
 end
