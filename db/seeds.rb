8bdd891b258d5720cef9f8c28aa203f022a42d5f
bolliveira <boliveira@4tuna.com.br>
2017-04-13 13:59:20 -0300

    some changes on seeds

--- db/seeds.rb
+++
@@ -1,3 +0,0 @@
-User.create!([
-  {provider: "twitter", uid: "2179015454", name: "Bruno Alexandre", email: "oliveira.desu@gmail.com", oauth_token: "2179015454-rRGumN0MccBv62zZyYsGEVwotWdbFDQfrjSZZBC", oauth_expires_at: nil, encrypted_password: "$2a$11$Y1KZPjwsbRBXNwM/8QWmPuFiZ5kILf6szVkLQatuixnCyQH8FcP.2", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil}
-])
